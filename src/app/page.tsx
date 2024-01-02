134e275111e55b1060da156d40328a266c5149dd
Wessel <wesselvdp@gmail.com>
2024-01-02 15:27:58 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,26 @@
-import Form from "@/components/Form";
+import CredentialsForm from "@/components/CredentialsForm";
+import { getServerSession } from "next-auth";
+import { getSubscriptions } from "@/lib/db";
 
-import Layout from "@/components/Layout";
+import { authConfig } from "@/lib/auth";
+import AuthWrapper from "@/components/AuthWrapper";
+import { SignIn } from "@/components/SignIn";
 export default async function Home() {
+  const session = await getServerSession(authConfig);
+
+  const subscriptions = await getSubscriptions();
+
   return (
-    <Layout>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <div className="w-full max-w-[600px]">
-          <Form />
-        </div>
-      </main>
-    </Layout>
+    <AuthWrapper>
+      <div className="flex justify-center items-center w-full h-[100vh] flex-col text-center">
+        <p>subscriptions:</p>
+        {/* <pre>{JSON.stringify(subscriptions, null, 2)}</pre> */}
+
+        <h3 className="text-2xl bold mb-2">Welcome stranger</h3>
+        <SignIn />
+        {/* <CredentialsForm /> */}
+        <pre>{JSON.stringify(session, null, 2)}</pre>
+      </div>
+    </AuthWrapper>
   );
-  // return (
-  //   <main className="flex min-h-screen flex-col items-center justify-between p-24">
-  //     <div className="w-full max-w-[600px]">
-  //       <Form />
-  //       <Subscriptions subscriptions={subscriptions} />
-  //     </div>
-  //   </main>
-  // );
 }
