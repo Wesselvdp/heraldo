9c87f89af7b1228603f9f13741473953151634c2
Wessel <wesselvdp@gmail.com>
2024-01-14 13:43:06 +0100

    Update CredentialsForm.tsx

--- src/components/CredentialsForm.tsx
+++ src/components/CredentialsForm.tsx
@@ -3,11 +3,14 @@ import React, { FC } from "react";
 import { pb } from "@/lib/pb";
 import Button from "./Button";
 import Image from "next/image";
+import { useRouter } from "next/router";
 type T = {
   login: (token: any, model: any) => void;
 };
 
 const CredentialsForm: FC<T> = ({ login }) => {
+  const router = useRouter();
+
   return (
     <div className="flex flex-col">
       {/* <Button
@@ -25,8 +28,9 @@ const CredentialsForm: FC<T> = ({ login }) => {
             .collection("users")
             .authWithOAuth2({ provider: "google" });
 
-          console.log({ token, model });
+          // console.log({ token, model });
           login(token, model);
+          router.push("/subscriptions");
         }}
       >
         <Image width="25" height="25" alt="google logo" src={"/google.png"} />
