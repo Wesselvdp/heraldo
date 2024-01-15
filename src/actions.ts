f78aad5316fed670654ddb6783ab0e01404a418b
Wessel <wesselvdp@gmail.com>
2024-01-15 11:37:23 +0100

    Update actions.ts

--- src/actions.ts
+++ src/actions.ts
@@ -8,11 +8,8 @@ export async function login(token: string, model: AuthModel) {
   try {
     const cookieres = cookies().set("pb_auth", cookie, {
       secure: true,
-      path: "/",
-      domain: "chamonix.netlify.app/",
-      sameSite: "lax",
-      httpOnly: true,
-      expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7 * 365 * 10)
+      domain: "chamonix.netlify.app",
+      sameSite: "lax"
     });
 
     console.log({ cookieres });
